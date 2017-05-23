54b91620f45bd1e57084198936d079795011cf7c
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-05-23 10:09:27 -0600

    Project cleanup

---
+++ ProgramaticConstraints/ProgramaticConstraints/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ProgramaticConstraints
+//
+//  Created by handje on 5/23/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
